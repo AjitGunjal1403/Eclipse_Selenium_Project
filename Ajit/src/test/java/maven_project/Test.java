853baaa51254a19dce1ba0135be1babc0bd4c2b7
Ajit <Ajit@DESKTOP-ER76S99>
2023-02-19 09:01:02 +0530

    Original Project

--- Ajit/src/test/java/maven_project/Test.java
+++ Ajit/src/test/java/maven_project/Test.java
@@ -8,21 +8,8 @@ import org.openqa.selenium.edge.EdgeDriver;
 
 public class Test {
 	public static void main(String[] args) {
-//		System.setProperty("webdriver.edge.driver","D:\\Automation - Selinium\\msedgedriver.exe");
-//		WebDriver driver = new EdgeDriver();
-//		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
-//		driver.manage().window().maximize();
-//		driver.get("https://www.facebook.com/");
 		
-		
-// 		System.setProperty("webdriver.edge.driver","D:\\Automation - Selinium\\msedgedriver.exe");
-// 		WebDriver driver = new EdgeDriver();
-// 		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
-// 		driver.manage().window().maximize();
-// 		driver.get("https://www.facebook.com/");
-		
-		
-	       System.setProperty("webdriver.edge.driver","D:\\Automation - Selinium\\msedgedriver.exe");
+	    System.setProperty("webdriver.edge.driver","D:\\Automation - Selinium\\msedgedriver.exe");
 		WebDriver driver = new EdgeDriver();
 		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
 		driver.manage().window().maximize();
