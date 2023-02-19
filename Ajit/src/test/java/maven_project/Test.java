da09af5ab8bd9f7d6931418c9831aa16ab4c9a29
AjitGunjal1403 <125779491+AjitGunjal1403@users.noreply.github.com>
2023-02-19 15:35:33 +0530

    Update Test.java

--- Ajit/src/test/java/maven_project/Test.java
+++ Ajit/src/test/java/maven_project/Test.java
@@ -27,6 +27,32 @@ public class Test {
 		driver.manage().window().maximize();
 		driver.get("https://www.facebook.com/");
 		
+		System.setProperty("webdriver.edge.driver","D:\\Automation - Selinium\\msedgedriver.exe");
+		WebDriver driver = new EdgeDriver();
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+		
+		System.setProperty("webdriver.edge.driver","D:\\Automation - Selinium\\msedgedriver.exe");
+		WebDriver driver = new EdgeDriver();
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+		
+		System.setProperty("webdriver.edge.driver","D:\\Automation - Selinium\\msedgedriver.exe");
+		WebDriver driver = new EdgeDriver();
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+		
+		System.setProperty("webdriver.edge.driver","D:\\Automation - Selinium\\msedgedriver.exe");
+		WebDriver driver = new EdgeDriver();
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+		
+		
+		
 	}
 	
 	}
